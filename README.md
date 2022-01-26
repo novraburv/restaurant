3e0892a37a5915199f8c6558857b604ed6dee75d
novraburv <78609440+novraburv@users.noreply.github.com>
2022-01-26 17:46:53 +0700

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# The Odin Project
+## Restaurant App
+
+Create a restaurant app. Generate most components of the app with DOM, and pack them with webpack.
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
+      filename: 'index.html',
+      title: 'Restaurant App'
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin()
+  ]
+};
+
+module.exports = config;
